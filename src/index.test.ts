f4127d6eb7fd22d5335428689d096c8d8982aea0
Jacob Hands <jacob@jacobhands.com>
2025-04-14 06:18:31 -0500

    switch to zod 3 temporarily

--- src/index.test.ts
+++ src/index.test.ts
@@ -5,21 +5,21 @@ import { env } from 'cloudflare:test';
 test('validate bindings', async () => {
 	// had to switch from z.object() to z.interface() after moving to Zod 4
 	// because z.function() can't be used in objects for some reason??
-	const DOBindingSchema = z.interface({
+	const DOBindingSchema = z.object({
 		get: z.function(),
 		idFromName: z.function(),
 		idFromString: z.function(),
 	});
 
-	const BindingsSchema = z.interface({
+	const BindingsSchema = z.object({
 		MY_DURABLE_OBJECT: DOBindingSchema,
 	});
 
 	// not trying to extract the DO binding using Zod, I just want to validate
 	// that the shape looks more or less correct
 
-	expect(BindingsSchema.safeParse(env)).toBe(false); // should be true, but doesn't work in zod 4
+	expect(BindingsSchema.safeParse(env).success).toBe(true); // should be true, but doesn't work in zod 4
 
 	// here's the error we get:
-	expect(() => BindingsSchema.parse(env)).toMatchInlineSnapshot();
+	// expect(() => BindingsSchema.parse(env)).toThrowErrorMatchingInlineSnapshot();
 });
