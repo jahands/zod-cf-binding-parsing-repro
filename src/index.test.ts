cb2b7eab4c78b3878096025c81df8fd40c077e90
Jacob Hands <jacob@jacobhands.com>
2025-04-14 06:09:05 -0500

    recreate from template

--- src/index.test.ts
+++ src/index.test.ts
@@ -1,25 +1,25 @@
-import { expect, test } from "vitest"
-import { z } from "zod"
-import { env } from "cloudflare:test"
+import { expect, test } from 'vitest';
+import { z } from 'zod';
+import { env } from 'cloudflare:test';
 
-test("validate bindings", async () => {
-  // had to switch from z.object() to z.interface() after moving to Zod 4
-  // because z.function() can't be used in objects for some reason??
-  const DOBindingSchema = z.interface({
-    get: z.function(),
-    idFromName: z.function(),
-    idFromString: z.function(),
-  })
+test('validate bindings', async () => {
+	// had to switch from z.object() to z.interface() after moving to Zod 4
+	// because z.function() can't be used in objects for some reason??
+	const DOBindingSchema = z.interface({
+		get: z.function(),
+		idFromName: z.function(),
+		idFromString: z.function(),
+	});
 
-  const BindingsSchema = z.interface({
-    MY_DURABLE_OBJECT: DOBindingSchema,
-  })
+	const BindingsSchema = z.interface({
+		MY_DURABLE_OBJECT: DOBindingSchema,
+	});
 
-  // not trying to extract the DO binding using Zod, I just want to validate
-  // that the shape looks more or less correct
+	// not trying to extract the DO binding using Zod, I just want to validate
+	// that the shape looks more or less correct
 
-  expect(BindingsSchema.safeParse(env)).toBe(false) // should be true, but doesn't work in zod 4
+	expect(BindingsSchema.safeParse(env)).toBe(false); // should be true, but doesn't work in zod 4
 
-  // here's the error we get:
-  expect(() => BindingsSchema.parse(env)).toMatchInlineSnapshot()
-})
+	// here's the error we get:
+	expect(() => BindingsSchema.parse(env)).toMatchInlineSnapshot();
+});
